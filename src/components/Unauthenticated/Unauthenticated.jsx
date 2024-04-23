e9988a281b9c0c04605bec6e6e37d01f715224bb
carusi99 <paolapachecocarusi@gmail.com>
2024-04-23 18:32:53 -0500

    component Unauthenticated

---
+++ src/components/Unauthenticated/Unauthenticated.jsx
@@ -0,0 +1,103 @@
+import * as React from "react";
+import s from "./Unauthenticated.module.css";
+import { useAuth } from "../../contexts/authContext";
+import Button from "../Button/Button"
+
+function Unauthenticated() {
+  const { login, signup } = useAuth();
+
+  const [status, setStatus] = React.useState("idle");
+  const [activeTab, setActiveTab] = React.useState("login");
+  const [signUpErrors, setSignUpErrors] = React.useState(null);
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // obtener datos del formulario
+    const formData = {
+      email,
+      password,
+    };
+
+    setStatus("loading");
+
+    if (activeTab === "login") {
+      login(formData.email, formData.password)
+        .then(() => setStatus("success"))
+        .catch(() => setStatus("error"));
+    } else {
+      signup(formData.email, formData.password)
+        .then(() => setStatus("success"))
+        .catch((error) => {
+          setStatus("error");
+          setSignUpErrors(error.message);
+        });
+    }
+  }
+
+  function handleTabChange(tab) {
+    setActiveTab(tab);
+    setStatus("idle");
+  }
+
+  const isLoading = status === "loading";
+  const buttonText = activeTab === "login" ? "Enter" : "Create";
+  const hasError = status === "error";
+
+  return (
+    <div className={s.wrapper}>
+      <div className={s.tabs}>
+        <button
+          onClick={() => handleTabChange("login")}
+          className={activeTab === "login" ? s.active : ""}
+        >
+          Login
+        </button>
+        <button
+          onClick={() => handleTabChange("signup")}
+          className={activeTab === "signup" ? s.active : ""}
+        >
+          Signup
+        </button>
+      </div>
+      <form onSubmit={handleSubmit} className={s.form}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="user@example.com"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            minLength={6}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <Button variant="primary" type="submit" disabled={isLoading}>
+          {isLoading ? "Loading..." : buttonText}
+        </Button>
+      </form>
+      {hasError && (
+        <p className={s["error-message"]}>
+          {signUpErrors || "Invalid Credentials"}
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default Unauthenticated;
